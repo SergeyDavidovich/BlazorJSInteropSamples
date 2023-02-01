aa3632f379aea96da4ae69838db0d25112933865
Sergiusz Kriwobokow <writesd@hotmail.com>
2023-02-02 00:21:19 +0100

    no comments

--- BlazorJSInteropSamples/wwwroot/scripts/app.js
+++ BlazorJSInteropSamples/wwwroot/scripts/app.js
@@ -1,1 +1,9 @@
 ﻿
+function Hide() {
+    document.getElementById('counter').style.visibility = "hidden";
+}
+function Show() {
+    document.getElementById('counter').innerText = "OK!";
+    document.getElementById('counter').style.color = "red";
+    document.getElementById('counter').style.visibility = "visible";
+}
